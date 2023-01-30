c9d3a30cae18e7f6bdae6bddb2cb512c322104c4
khaled311 <k.sarhan311@gmail.com>
2023-01-30 14:56:31 +0200

    add $ sign to the price

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -21,7 +21,7 @@ const Product = ({ product: { name, image, slug, price } }: Props) => {
             className="product-image"
           />
           <p className="product-name">{name}</p>
-          <p className="product-price">{price}$</p>
+          <p className="product-price">${price}</p>
         </div>
       </Link>
     </div>
